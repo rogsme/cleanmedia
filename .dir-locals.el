7106991a04aaba60127a2adb043f385c97ff44f6
Roger Gonzalez <roger@rogs.me>
2024-12-04 11:45:23 -0300

    Enable specific LSP plugins in Emacs dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((python-mode . ((lsp-pylsp-plugins-ruff-enabled . t)
+                 (lsp-pylsp-plugins-pydocstyle-enabled . nil)
+                 (lsp-pylsp-plugins-flake8-enabled . nil)
+                 (lsp-pylsp-plugins-mypy-enabled . t))))
